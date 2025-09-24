71ac41c8912cfa568efeda3a1eb9a8653ebebcc5
Maria <mariaalejan1080@gmail.com>
2025-09-24 14:27:47 -0500

    Borrar campo de evaluacion resul

--- Entity/Dtos/ModuleOperation/CreateEvaluation/EvaluationRegisterDTO.cs
+++ Entity/Dtos/ModuleOperation/CreateEvaluation/EvaluationRegisterDTO.cs
@@ -8,7 +8,7 @@ namespace Entity.Dtos.ModuleOperation.CreateEvaluation
         public string TypeEvaluation { get; set; } = string.Empty;
         public string AccompanimentRole { get; set; } = string.Empty;
         public string Comments { get; set; } = string.Empty;
-        public string EvaluationResult { get; set; } = string.Empty;
+//      public string EvaluationResult { get; set; } = string.Empty;
 
         public int UserId { get; set; }
         public int ExperienceId { get; set; }
